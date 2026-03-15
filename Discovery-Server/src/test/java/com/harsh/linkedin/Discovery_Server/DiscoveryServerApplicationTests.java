86edf27ce944d70850059b4038cc535fb873cc4a
Harsh Choudhary <harshchoudhary2103j@gmail.com>
2026-03-15 19:55:34 +0530

    Added Discovery Server and API-Gateway, Registered Post and User Service with API-Gateway

---
+++ Discovery-Server/src/test/java/com/harsh/linkedin/Discovery_Server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.linkedin.Discovery_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
